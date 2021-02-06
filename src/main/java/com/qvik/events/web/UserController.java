169ebdf7be10174c9fe4ce63a0c55fbfa392528d
taeheelee90 <taehee.lee90@gmail.com>
2021-02-06 17:56:36 +0200

    06.02.2021 - Events calls are edited - Tei

--- src/main/java/com/qvik/events/web/UserController.java
+++ src/main/java/com/qvik/events/web/UserController.java
@@ -1,20 +1,30 @@
 package com.qvik.events.web;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qvik.events.domain.Event;
 import com.qvik.events.domain.EventExhibitorRepository;
 import com.qvik.events.domain.EventPresenterRepository;
 import com.qvik.events.domain.EventRepository;
 import com.qvik.events.domain.EventRestaurantRepository;
 import com.qvik.events.domain.EventStageRepository;
 import com.qvik.events.domain.EventVenueRepository;
+import com.qvik.events.domain.Event_Exhibitor;
+import com.qvik.events.domain.Event_Presenter;
+import com.qvik.events.domain.Event_Restaurant;
+import com.qvik.events.domain.Event_Stage;
+import com.qvik.events.domain.Event_Venue;
 import com.qvik.events.domain.ExhibitorRepository;
 import com.qvik.events.domain.PresenterRepository;
 import com.qvik.events.domain.RestaurantRepository;
@@ -40,6 +50,7 @@ public class UserController {
 	private final StageRepository stageRepository;
 	private final VenueRepository venueRepository;
 
+	
 	@GetMapping(path = "/")
 	public String root() throws JsonProcessingException {
 		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
@@ -50,46 +61,134 @@ public class UserController {
 		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
 	}
 
-	@GetMapping(path = "/events-info")
-	public String eventsInfo(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
-	}
-
 	@GetMapping(path = "/events-date")
 	public String eventsDate(@RequestParam(name = "startDate") Date startDate) throws JsonProcessingException {
 		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
 	}
+	/*
+	 * @GetMapping(path = "/events-info") public String
+	 * eventsInfo(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 * 
+	 * @GetMapping(path = "/events-stage") public String
+	 * eventsStage(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 * 
+	 * @GetMapping(path = "/events-venue") public String
+	 * eventsVenue(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 * 
+	 * @GetMapping(path = "/events-exhibitor") public String
+	 * eventsExhibitor(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 * 
+	 * @GetMapping(path = "/events-presenter") public String
+	 * eventsPresenter(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 * 
+	 * @GetMapping(path = "/events-description") public String
+	 * eventsDescription(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 * 
+	 * @GetMapping(path = "/events-restaurant") public String
+	 * eventsRestaurant(@RequestParam(name = "eventId") Integer eventId) throws
+	 * JsonProcessingException { return new
+	 * ObjectMapper().writeValueAsString(eventRepository.findAll()); }
+	 */
 
-	@GetMapping(path = "/events-stage")
-	public String eventsStage(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+	
+	/*
+	 * TODO: CREATE EVENTSERVICE to handle search from DB
+	 * TODO: CREATE MAPPER for response body
+	 * TODO: EXCEPTION HANDLING
+	 * 
+	 */
+	@GetMapping(path = "/events/{eventId}")
+	public ResponseEntity<Event> eventsInfo(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get());
 	}
 
-	@GetMapping(path = "/events-venue")
-	public String eventsVenue(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+	@GetMapping(path = "/events/{eventId}/stages")
+	public ResponseEntity<List<Event_Stage>> eventsStage(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get().getEvent_stages());
 	}
 
-	@GetMapping(path = "/events-exhibitor")
-	public String eventsExhibitor(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+	@GetMapping(path = "/events/{eventId}/venues")
+	public ResponseEntity<List<Event_Venue>> eventsVenue(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get().getEvent_venues());
 	}
 
-	@GetMapping(path = "/events-presenter")
-	public String eventsPresenter(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+	@GetMapping(path = "/events/{eventId}/exhibitors")
+	public ResponseEntity<List<Event_Exhibitor>> eventsExhibitor(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get().getEvent_exhibitors());
 	}
 
-	@GetMapping(path = "/events-description")
-	public String eventsDescription(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+	@GetMapping(path = "/events/{eventId}/presenters")
+	public ResponseEntity<List<Event_Presenter>> eventsPresenter(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get().getEvent_presenters());
 	}
 
-	@GetMapping(path = "/events-restaurant")
-	public String eventsRestaurant(@RequestParam(name = "eventId") Integer eventId) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+	@GetMapping(path = "/events/{eventId}/full-description")
+	public ResponseEntity<String> eventsFullDescription(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get().getFull_description());
 	}
 
+	@GetMapping(path = "/events/{eventId}/restaurants")
+	public ResponseEntity<List<Event_Restaurant>> eventsRestaurant(@PathVariable Long eventId) {
+		HttpStatus status = HttpStatus.OK;
+		Optional<Event> foundEvent = eventRepository.findById(eventId);
+		if (!foundEvent.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+			throw new IllegalArgumentException();
+		}
+		return ResponseEntity.status(status).body(foundEvent.get().getEvent_restaurants());
+	}
+
+	/*
+	 * TODO: Separate CONTROLLER
+	 */
 	@GetMapping(path = "/venues-info")
 	public String venusInfo(@RequestParam(name = "venueId") Integer venueId) throws JsonProcessingException {
 		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
@@ -124,7 +223,7 @@ public class UserController {
 
 	@GetMapping(path = "/restaurants")
 	public String restaurants() throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(eventRepository.findAll());
+		return new ObjectMapper().writeValueAsString(restaurantRepository.findAll());
 	}
 
 	@GetMapping(path = "/restaurants-info")
