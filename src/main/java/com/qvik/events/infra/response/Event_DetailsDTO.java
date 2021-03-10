d0a2aed340488a62e37488764508e8ce5cdcd65a
taeheelee90 <taehee.lee90@gmail.com>
2021-03-10 14:23:50 +0200

    10.03.2021 - Return event detail location with ID - Tei

--- src/main/java/com/qvik/events/infra/response/Event_DetailsDTO.java
+++ src/main/java/com/qvik/events/infra/response/Event_DetailsDTO.java
@@ -3,16 +3,22 @@ package com.qvik.events.infra.response;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.qvik.events.modules.stage.Stage;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL) 
 public class Event_DetailsDTO extends Event_BaseDTO {
 	
 	private String image;
 	private String shortDescription;
 	private String fullDescription;	
 
-	//private Set<Event_PresenterDTO> eventPresenters = new HashSet<>();	
+	//private Set<Event_PresenterDTO> eventPresenters = new HashSet<>();
+	private VenueDTO venue;
+	private StageDTO stage;
 	private List<String> presenters = new ArrayList<>();
 
 }
